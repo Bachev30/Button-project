91f9b53431a91d8c3131abc06f94f7e22fc379df
Bachev30 <102502459+Bachev30@users.noreply.github.com>
2024-01-04 09:56:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixxHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
